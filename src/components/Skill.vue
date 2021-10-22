79176ed4d4fe7a6918e8a9ccc899c7dfa092233c
David <raymondinf23@gmail.com>
2021-10-21 23:35:05 -0400

    Create Skill component

---
+++ src/components/Skill.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+        class="flex flex-col justify-center shadow-md hover:shadow-lg hover:translate-x-96
+              border-transparent border-2 rounded-lg p-2 w-10/12 dark:bg-blue-100"
+    >
+        <img :src="skill.image" alt="Skill image" class="w-24 self-center">
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    skill: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
