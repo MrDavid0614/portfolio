f56c434d7c8e00f04331c4bb0df5b230ec857f55
David <raymondinf23@gmail.com>
2021-10-21 23:31:41 -0400

    Add global style to main js file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import store from '@/store'
 import App from './App.vue'
 import router from './router'
+import './assets/styles.css'
 import './assets/tailwind.css'
 
 createApp(App)
