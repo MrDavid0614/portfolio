e1cdd79d46db5b2216e8764e8573c385dc0ef4ef
David <raymondinf23@gmail.com>
2021-10-21 23:34:02 -0400

    Create Hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="flex justify-center items-center h-screen">
+      <div>
+          <h1 class="text-6xl text-blue-100">Hey! I am Raymond Mora</h1>
+          <p class="text-4xl text-blue-300">A Front-End Web Developer</p>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+section {
+    background-image: linear-gradient(
+        62deg, #3a3d40 0%, #181719 100%);
+}
+
+</style>
