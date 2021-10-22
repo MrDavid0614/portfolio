7f96cd8664d2fa5590f2046a25edf11a9306a8c7
David <raymondinf23@gmail.com>
2021-10-21 23:35:28 -0400

    Import Skill component in Skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,18 +1,27 @@
 <template>
-    <ul>
-        <li
-            v-for="skill in skills"
-            :key="skill.id"
-        >{{ skill.name }}</li>
-    </ul>
+  <h1 class="mt-20 text-5xl">My Skills</h1>
+  <section class="flex justify-center m-10">
+    <div class="grid grid-cols-3 gap-y-5 gap-x-2">
+      <Skill
+        v-for="skill in skills"
+        :key="skill.id"
+        :skill="skill"
+      />
+    </div>
+  </section>
 </template>
 
 <script>
+import Skill from './Skill.vue'
+
 export default {
   computed: {
     skills () {
       return this.$store.state.skills
     }
+  },
+  components: {
+    Skill
   }
 }
 </script>
