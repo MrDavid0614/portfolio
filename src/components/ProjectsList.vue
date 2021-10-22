e7ddd71930a057c61330dab5a82e2554ad2f9a14
David <raymondinf23@gmail.com>
2021-10-21 23:35:43 -0400

    Style projects list

--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="grid grid-cols-3 gap-5">
-    <ProjectsListItem
-      v-for="project in projects"
-      :key="project.id"
-      :project="project"
-    />
-  </div>
+  <section class="bg-blue-100 mt-32 p-6">
+    <h1 class="text-center text-5xl">My Projects</h1>
+    <section class="grid grid-cols-3 gap-5" id="projects">
+      <ProjectsListItem
+        v-for="project in projects"
+        :key="project.id"
+        :project="project"
+      />
+    </section>
+  </section>
 </template>
 
 <script>
