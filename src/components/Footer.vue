770567c93a42fc5a139cf8d038dd94e7d54bd761
David <raymondinf23@gmail.com>
2021-10-21 23:32:19 -0400

    Create Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <p>
+        Made by
+        <a
+            href="https://linkedin.com/in/raymondmora"
+            target="_blank"
+            class="underline text-blue-600 hover:text-blue-800"
+        >
+            Raymond Mora
+        </a>
+        with Vue.js💚
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
